0383fed9438d8e372d432b647f316b3f40eeccbd
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-07 18:08:37 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    self.ends_with?(".")
+    self.end_with?(".")
   end
 
   def question?
-    self.ends_with?("?")
+    self.end_with?("?")
   end
 
   def exclamation?
-    self.ends_with?("!")
+    self.end_with?("!")
   end
 
   def count_sentences
